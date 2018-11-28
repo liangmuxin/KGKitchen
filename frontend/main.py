bc2d2eed37d136bb48b322d418aad91d3ce299e9
Yuqiu He <yyyuqiu@gmail.com>
2018-11-28 13:16:07 -0800

    frontend

---
+++ frontend/main.py
@@ -0,0 +1,125 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+from flask import Flask,render_template,request
+from query import QUERY
+
+app = Flask(__name__)
+dish_types = ['Appetizers & Snacks', 'Bread Recipes', 'Cake Recipes', 'Candy and Fudge', 'Casserole Recipes',
+              'Christmas Cookies', 'Cocktail Recipes', 'Cookie Recipes', 'Mac and Cheese Recipes', 'Main Dishes',
+              'Pasta Salad Recipes', 'Pasta Recipes', 'Pie Recipes', 'Pizza', 'Sandwiches', 'Sauces and Condiments',
+              'Smoothie Recipes', 'Soups, Stew, and Chili Recipes']
+def datalist_ingredients():
+    ingredients = []
+    for line in open('food_types.txt', 'r'):
+        ingredients.append(line.strip('\n').replace('% ',''))
+    return ingredients
+
+
+def sparql_ingre(ingredient):
+    sparql = SPARQLWrapper("http://localhost:3030/test/sparql")
+    query1 = QUERY(sparql, JSON, ["food_name", "food_unit_price", "food_unit", "food_price"],
+                   _filter=[("topclass", ingredient)], distinct=True)
+    query2 = QUERY(sparql, JSON, ["recipe_type", "recipe_name", "recipe_url" ], _filter=[("topclass", ingredient)])
+
+    results1 = query1.run_query()
+    results2 = query2.run_query()
+    return results1, results2
+
+
+def sparql_dish_ingre(dishes,ingredient):
+    sparql = SPARQLWrapper("http://localhost:3030/test/sparql")
+    results1 = []
+    flag = 1
+    for ig in ingredient:
+        if dishes == '' and ig != '':
+            query = QUERY(sparql, JSON, ["recipe_name", "ingredient", "recipe_url"],
+                          _filter=[("topclass", ig)], limit=15)
+        elif dishes != '' and ig == '':
+            query = QUERY(sparql, JSON, ["recipe_name", "ingredient", "recipe_url"],
+                          _filter=[("recipe_type", dishes)], distinct=True, limit=15)
+        elif dishes == '' and ig == '':
+            query = QUERY(sparql, JSON, ["recipe_name", "ingredient", "recipe_url"], limit=15)
+        else:
+            query = QUERY(sparql, JSON, ["recipe_name", "ingredient", "recipe_url"],
+                  _filter=[("topclass",ig),("recipe_type", dishes)], limit=15)
+        results1.append(query.run_query())
+
+    results2 = []
+    url,food_name,origin_ingre = [],[],[]
+    for results in results1:
+        for i in range(len(results['results']['bindings'])):
+            url.append(results['results']['bindings'][i]['recipe_url']['value'])
+            food_name.append(results['results']['bindings'][i]['recipe_name']['value'])
+            ingredients_return = results['results']['bindings'][i]['ingredient']['value'].strip('[').strip(']').replace(', ','').replace('\"','').split('\'')
+            while '' in ingredients_return:
+                ingredients_return.remove('')
+            origin_ingre.append(ingredients_return)
+
+            query = QUERY(sparql, JSON, ["food_name", "amount", "food_unit_price", "food_unit", "topclass"],
+                   _filter=[("recipe_url",results['results']['bindings'][i]['recipe_url']['value'])], limit=15)
+            results2.append(query.run_query())
+
+    return results2,url,food_name,origin_ingre
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', Ingredients = datalist_ingredients(), disTypes = dish_types)
+
+
+@app.route('/searchByRecipe',methods=['POST','GET'])
+def searchByRecipe():
+    if request.method == 'POST':
+        dish_type = request.form['dishType']
+        ingredient_ori = request.form['ingredient']
+        ingredient = ingredient_ori.split(',')
+        results2, url, food_name, origin_ingre = sparql_dish_ingre(dish_type,ingredient)
+
+        return_dict = []
+        j = 0
+        for results in results2:
+            ingredients, unit_price_list, food_unit, topclass, amount_list = [], [], [], [], []
+            total = 0
+            for i in range(len(results['results']['bindings'])):
+                ingredients.append(results['results']['bindings'][i]['food_name']['value'])
+                food_unit.append(results['results']['bindings'][i]['food_unit']['value'])
+                unit_price = eval(results['results']['bindings'][i]['food_unit_price']['value'])
+                amount = eval(results['results']['bindings'][i]['amount']['value'])
+                topclass.append(results['results']['bindings'][i]['topclass']['value'])
+                unit_price_list.append(unit_price)
+                amount_list.append(amount)
+                total += (unit_price * amount)
+            total = round(total * 100)/100
+            return_dict.append({"url":url[j],"food_name": food_name[j], "origin_ingre": origin_ingre[j],"ingredients_amazon": ingredients, "unit_price": unit_price_list,"food_unit":food_unit,"total":total,"topclass":topclass,"amount_list":amount_list})
+            j += 1
+
+
+        return render_template('searchByRecipe.html', Ingredients = datalist_ingredients(), disTypes = dish_types, returnDict = return_dict[0:15], getDishType = dish_type, getIngred = ingredient_ori)
+
+
+@app.route('/searchByFood',methods=['POST','GET'])
+def searchByFood():
+    if request.method == 'POST':
+        ingredient = request.form['ingredient']
+        results1, results2 = sparql_ingre(ingredient)
+        foodName,food_unit_price,food_unit,food_price,re_type,re_name,re_url = [], [], [], [], [], [], []
+        return_dict,re_dict_2 = [], []
+
+        print(results2)
+        for i in range(len(results1['results']['bindings'])):
+            foodName.append(results1['results']['bindings'][i]['food_name']['value'])
+            food_unit_price.append(eval(results1['results']['bindings'][i]['food_unit_price']['value']))
+            food_unit.append(results1['results']['bindings'][i]['food_unit']['value'])
+            food_price.append(eval(results1['results']['bindings'][i]['food_price']['value']))
+            return_dict.append({"foodName":foodName,"food_unit_price":food_unit_price,"food_unit":food_unit,"food_price":food_price})
+        for i in range(len(results2['results']['bindings'])):
+            re_type.append(results2['results']['bindings'][i]['recipe_type']['value'])
+            re_name.append(results2['results']['bindings'][i]['recipe_name']['value'])
+            re_url.append(results2['results']['bindings'][i]['recipe_url']['value'])
+            re_dict_2.append({"re_type":re_type,"re_name":re_name,"re_url":re_url})
+
+        return render_template('searchByFood.html', Ingredients=datalist_ingredients(), disTypes=dish_types,
+                               ingredient=ingredient, returnDict=return_dict, returnDict2 = re_dict_2)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ frontend/query.py
@@ -0,0 +1,100 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+class QUERY:
+    def __init__(self, sparql: SPARQLWrapper, return_format, selection: list, *args, **args_val):
+        self.sparql = sparql
+        self.return_format = return_format
+        self.SELECTION_LIST = list(map(lambda x: "?"+x, selection))
+        if "distinct" in args_val:
+            self.DISTINCT = "distinct"
+        else:
+            self.DISTINCT = ""
+        if "limit" in args_val:
+            self.LIMIT = "LIMIT" + str(args_val["limit"])
+        else:
+            self.LIMIT = ""
+        if "_filter" in args_val:
+            self.FILTER = list(map(lambda x: "?"+x[0]+"="+"\""+str(x[1])+"\"", args_val["_filter"]))
+        else:
+            self.FILTER = None
+        self.query = None
+        self.PREFIX = """
+        PREFIX qb: <http://purl.org/linked-data/cube#>
+        PREFIX inf558: <http://inf558.org/ns/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX schema: <http://schema.org/>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        """
+        self.STRUCTURE = """
+        where {
+            ?u a schema:usedby; 
+                schema:amount ?amount ;
+                schema:unit ?using_unit ;
+                schema:unit_price ?using_unit_price ;
+                schema:inrecipe ?R ;
+                schema:forfood ?A . 
+            ?A a schema:food ;
+                schema:topclass ?topclass ;
+                schema:subclass ?subclass ;
+                rdf:value ?food_name ;
+                schema:price ?food_price ;
+                schema:unit ?food_unit ;
+                schema:unit_price ?food_unit_price .
+            ?R a schema:recipe ;
+                schema:recipe_url ?recipe_url ;
+                schema:recipe_type ?recipe_type ;
+                schema:type_url ?type_url ;
+                schema:ingredient ?ingredient ;
+                rdf:value ?recipe_name ;
+        """
+    def run_query(self, verbose = False):
+        SELECTION = """
+            SELECT {}
+        """.format(self.DISTINCT + " ".join(self.SELECTION_LIST))
+        if not self.FILTER:
+            FILTER = ""
+        else:
+            FILTER = ""
+            for f in self.FILTER:
+                tmp = "filter("+f+")"
+                FILTER += tmp
+        self.sparql.setQuery(self.PREFIX+SELECTION+self.STRUCTURE+"\n"+FILTER+"\n"+"}"+"{}".format(self.LIMIT))
+        if verbose:
+            print(self.PREFIX+SELECTION+self.STRUCTURE+"\n"+FILTER+"\n"+"}"+"{}".format(self.LIMIT))
+        self.sparql.setReturnFormat(JSON)
+        return self.sparql.query().convert()
+
+
+def main():
+    sparql = SPARQLWrapper("http://localhost:3030/test/sparql")
+    query1 = QUERY(sparql, JSON, ["ingredient", "recipe_name"], limit=10)
+    # print(query1.run_query())
+    query2 = QUERY(sparql, JSON, ["food_name", "topclass", "unit_price"], limit=10)
+    # print(query2.run_query())
+    query3 = QUERY(sparql, JSON, ["recipe_name", "ingredient", "recipe_url"], _filter=[("topclass", "milk")], limit=10)
+    query4 = QUERY(sparql, JSON, ["food_name", "amount", "unit_price", "food_unit", "topclass"], _filter=[("recipe_url", "https://www.allrecipes.com/recipe/75194/fresh-veggie-bagel-sandwich/")])
+    query5 = QUERY(sparql, JSON, ["food_name", "food_unit_price", "food_unit", "food_price"], _filter=[("topclass", "mushroom")], distinct=True)
+    query6 = QUERY(sparql, JSON, ["recipe_type", "recipe_name"], _filter=[("topclass", "mushroom")])
+    # estimating cost
+    res = query4.run_query()
+    for v in res["results"]["bindings"]:
+        for k in v.keys():
+            print(v[k]["value"])
+    # res1 = query5.run_query()
+    # # import pdb
+    # # pdb.set_trace()
+    # for v in res1["results"]["bindings"]:
+    #     for k in v.keys():
+    #         print(v[k]["value"])
+    # res2 = query6.run_query()
+
+    # for v in res2["results"]["bindings"]:
+    #     for k in v.keys():
+    #         print(v[k]["value"])
+
+    print("finished")
+
+
+
+if __name__ == "__main__":
+    main()
